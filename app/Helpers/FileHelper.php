70adc12c8a9b8a457882a173182ad6e327487dea
Nick T-bone Kiermaier <nickkiermaier@gmail.com>
2018-01-02 11:13:42 -0600

    upd

--- app/Helpers/FileHelper.php
+++ app/Helpers/FileHelper.php
@@ -11,11 +11,13 @@ class FileHelper
      * Creates a directory
      * Awesome discussion here on various ways to create a directory in php:
      * https://stackoverflow.com/questions/2303372/create-a-folder-if-it-doesnt-already-exist
+     * (I didn't like the accepted answer because it doesnt allow creating a file and directory with the same name.
+     * as well as didn't allow setting permissions)
      * @param $path
      * @param int $mode
      * @return bool
      */
-    function makedirs($path, $mode = 0777)
+    function createDirectory($path, $mode = 0777)
     {
         return is_dir($path) || mkdir($path, $mode, true);
     }
